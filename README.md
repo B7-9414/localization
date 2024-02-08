af0e867b82617e76bdb7e0bda0575da56fdfa928
RohitBagda <rohitbagda@gmail.com>
2024-02-07 19:06:44 -0600

    Add Localization support for Arabic, Hindi and Spanish

---
+++ README.md
@@ -0,0 +1,6 @@
+The app dynamically greets the user based on the language settings of their device. If the device language is set to English, the greeting message will be displayed in English. Similarly, if the device language is set to Arabic, the greeting message will be displayed in Arabic.
+Supported Languages: The Android app supports the following languages:
+* English (default)
+* Arabic
+* Hindi
+* Spanish
--- app/src/main/java/com/example/localization/MainActivity.kt
+++ app/src/main/java/com/example/localization/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.localization
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.localization.ui.theme.LocalizationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +36,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = stringResource(R.string.hello))
+        Text(text = stringResource(R.string.welcome_title))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LocalizationTheme {
-        Greeting("Android")
-    }
-}
+fun GreetingPreview() = LocalizationTheme { Greeting() }
+@Preview(showBackground = true, locale = "ar")
+@Composable
+fun ArabicGreetingPreview() = LocalizationTheme { Greeting() }
+@Preview(showBackground = true, locale = "hi")
+@Composable
+fun HindiGreetingPreview() = LocalizationTheme { Greeting() }
+@Preview(showBackground = true, locale = "es")
+@Composable
+fun SpanishGreetingPreview() = LocalizationTheme { Greeting() }
